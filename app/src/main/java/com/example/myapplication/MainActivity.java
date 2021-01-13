b7eb2a8525964fad2321faaec1d4319a51eb276f
gary <garypyguo@tencent.com>
2021-01-13 16:52:20 +0800

    dev

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // test branch
+        // test dev
         setContentView(R.layout.activity_main);
     }
 }
