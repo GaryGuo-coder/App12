d1c5e8d5d94a5de9390271e393068f6ebeaa8b26
gary <garypyguo@tencent.com>
2021-01-13 16:20:06 +0800

    master

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // test branch
         setContentView(R.layout.activity_main);
+        // test master
     }
 }
